17a881794864eca0e18f07f2afd99046b486f028
er <er@sodah.net>
2024-07-04 09:30:06 -0400

    show headers

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return "HOSTNAME: " + os.environ.get('HOSTNAME', "N/A")
+  #return "HOSTNAME: " + os.environ.get('HOSTNAME', "N/A")
+  headers = list(request.headers)
+  headers.append(["server-hostname", os.environ.get('HOSTNAME', "N/A")])
+  return json.dumps(headers)
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=80)
+    app.run(debug=False, host="0.0.0.0", port=5000)
